413ef065350eb4c4bea465fc79b37777066c62ca
chaithanyakamath <nnm23is035@nmamit.in>
2026-04-16 12:22:40 +0530

    second

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: process.env.CLIENT_URL || "http://localhost:5174",
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
     methods: ["GET", "POST"],
   },
 });
